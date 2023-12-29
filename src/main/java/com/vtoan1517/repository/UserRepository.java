7891260ea22b740a7678028938f8cb7a0b8d7446
vantoan1511 <vantoan1517@gmail.com>
2023-12-29 17:39:31 +0700

    crud method return optional instead of entity

--- src/main/java/com/vtoan1517/repository/UserRepository.java
+++ src/main/java/com/vtoan1517/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.vtoan1517.repository;
 import com.vtoan1517.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
 }
