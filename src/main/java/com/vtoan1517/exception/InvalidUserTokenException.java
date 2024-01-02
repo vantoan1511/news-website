d38821bbf40ba86e7788f767e17122e072d2a59b
vantoan1511 <vantoan1517@gmail.com>
2024-01-02 16:53:20 +0700

    add InvalidUserTokenException

---
+++ src/main/java/com/vtoan1517/exception/InvalidUserTokenException.java
@@ -0,0 +1,7 @@
+package com.vtoan1517.exception;
+
+public class InvalidUserTokenException extends Exception {
+    public InvalidUserTokenException(String message) {
+        super(message);
+    }
+}
