3d152adba9ec3596b114e810a3a70f57ec206032
vantoan1511 <vantoan1517@gmail.com>
2024-01-02 16:52:59 +0700

    put view

--- src/main/java/com/vtoan1517/controller/web/HomeController.java
+++ src/main/java/com/vtoan1517/controller/web/HomeController.java
@@ -1,10 +1,15 @@
 package com.vtoan1517.controller.web;
 
+import com.vtoan1517.dto.ArticleDTO;
 import com.vtoan1517.service.IArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller(value = "WebHomeController")
 public class HomeController {
@@ -18,8 +23,21 @@ public class HomeController {
 
     @GetMapping("/home")
     public ModelAndView home() {
+
+        PageRequest featuredReq = new PageRequest(0, 3, new Sort(Sort.Direction.DESC, "modifiedDate"));
+        PageRequest latestReq = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "modifiedDate"));
+        PageRequest popularReq = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "traffic"));
+
+        List<ArticleDTO> featured = articleService.findAllByStatusCodeAndFeatured("published", true, featuredReq);
+        List<ArticleDTO> latest = articleService.findAllByStatusCode("published", latestReq);
+        List<ArticleDTO> popular = articleService.findAllByStatusCode("published", popularReq);
+
         ModelAndView mav = new ModelAndView("web/home");
         mav.addObject("latestArticles", articleService.findAll());
+        mav.addObject("featured", featured);
+        mav.addObject("latest", latest);
+        mav.addObject("popular", popular);
+
         return mav;
     }
 }
