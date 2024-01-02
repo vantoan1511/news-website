3affeacccfd59a3d3e599979a4ad55904cc005bd
vantoan1511 <vantoan1517@gmail.com>
2024-01-02 21:07:26 +0700

    optimize code

--- src/main/java/com/vtoan1517/controller/web/ErrorController.java
+++ src/main/java/com/vtoan1517/controller/web/ErrorController.java
@@ -1,6 +1,8 @@
 package com.vtoan1517.controller.web;
 
+import com.vtoan1517.utils.FlashMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ErrorHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -9,8 +11,8 @@ public class ErrorController {
 
     @GetMapping("/access-denied")
     public String error(RedirectAttributes attributes) {
-        attributes.addFlashAttribute("type", "danger");
-        attributes.addFlashAttribute("message", "Bạn không có quyền truy cập");
+        attributes.addFlashAttribute("message",
+                new FlashMessage(FlashMessage.DANGER, "Bạn không có quyền truy cập"));
         return "redirect:/login";
     }
 }
