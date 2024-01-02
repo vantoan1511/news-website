1aa83c95d852f35a4776c6065a732fe7887acc8f
vantoan1511 <vantoan1517@gmail.com>
2024-01-02 16:55:21 +0700

    add findByCode method

--- src/main/java/com/vtoan1517/repository/RoleRepository.java
+++ src/main/java/com/vtoan1517/repository/RoleRepository.java
@@ -4,4 +4,6 @@ import com.vtoan1517.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+
+    RoleEntity findByCode(String code);
 }
