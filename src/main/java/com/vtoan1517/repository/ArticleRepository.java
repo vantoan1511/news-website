e0abc9507327e561f5d06fb39895ea7d44d33d86
vantoan1511 <vantoan1517@gmail.com>
2024-01-02 16:50:26 +0700

    add findAllByStatusCodeAndFeatured method

--- src/main/java/com/vtoan1517/repository/ArticleRepository.java
+++ src/main/java/com/vtoan1517/repository/ArticleRepository.java
@@ -1,13 +1,13 @@
 package com.vtoan1517.repository;
 
 import com.vtoan1517.entity.ArticleEntity;
-import com.vtoan1517.entity.StatusEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+
 
     List<ArticleEntity> findAllByStatusCodeNot(String statusCode, Pageable pageable);
 
@@ -22,6 +22,8 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
     List<ArticleEntity> findAllByStatusCodeAndCreatedBy(String statusCode, String createdBy, Pageable pageable);
 
     List<ArticleEntity> findAllByFeatured(boolean featured, Pageable pageable);
+
+    List<ArticleEntity> findAllByStatusCodeAndFeatured(String statusCode, boolean featured, Pageable pageable);
 
     ArticleEntity findById(long id);
 
