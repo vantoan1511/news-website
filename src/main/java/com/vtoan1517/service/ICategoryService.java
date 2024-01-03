8a2e3a6fb922b2e352cf8579b9794ec575534875
vantoan1511 <vantoan1517@gmail.com>
2024-01-03 16:31:39 +0700

    add findByCode method

--- src/main/java/com/vtoan1517/service/ICategoryService.java
+++ src/main/java/com/vtoan1517/service/ICategoryService.java
@@ -1,6 +1,7 @@
 package com.vtoan1517.service;
 
 import com.vtoan1517.dto.CategoryDTO;
+import com.vtoan1517.exception.CategoryNotFoundException;
 
 import java.util.Map;
 
@@ -8,4 +9,6 @@ public interface ICategoryService {
     CategoryDTO save(CategoryDTO categoryDTO);
 
     Map<String, String> findAll();
+
+    CategoryDTO findByCode(String code) throws CategoryNotFoundException;
 }
