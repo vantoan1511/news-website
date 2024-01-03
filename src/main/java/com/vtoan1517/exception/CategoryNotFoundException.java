dcf6e4205619050a71d9bcc398a0385c2f4a4eb5
vantoan1511 <vantoan1517@gmail.com>
2024-01-03 16:33:27 +0700

    add ex

---
+++ src/main/java/com/vtoan1517/exception/CategoryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.vtoan1517.exception;
+
+public class CategoryNotFoundException extends Exception {
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
