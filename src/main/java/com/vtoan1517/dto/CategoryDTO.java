b98d47b21759d5110f4b706895df6b769f685ec3
vantoan1511 <vantoan1517@gmail.com>
2024-01-03 16:33:39 +0700

    update atrrs

--- src/main/java/com/vtoan1517/dto/CategoryDTO.java
+++ src/main/java/com/vtoan1517/dto/CategoryDTO.java
@@ -1,9 +1,13 @@
 package com.vtoan1517.dto;
 
+import com.vtoan1517.entity.Article;
+import com.vtoan1517.entity.Category;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -12,4 +16,7 @@ public class CategoryDTO extends BaseDTO {
     private String code;
     private String name;
     private String parentCode;
+    private String parentName;
+    private Set<CategoryDTO> subCategories = new HashSet<>();
+    private List<ArticleDTO> articles = new ArrayList<>();
 }
