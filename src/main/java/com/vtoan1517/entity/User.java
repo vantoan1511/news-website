8b5bbac19e90882616c36ee704f5d0fc939e324d
vantoan1511 <vantoan1517@gmail.com>
2024-01-09 11:54:18 +0700

    update code

--- src/main/java/com/vtoan1517/entity/User.java
+++ src/main/java/com/vtoan1517/entity/User.java
@@ -37,7 +37,7 @@ public class User extends Base {
     @Column(name = "avatar_url", columnDefinition = "TEXT")
     private String avatarUrl;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
