8c157caf1fc9d80fbfcf131a252942887bba66ec
vantoan1511 <vantoan1517@gmail.com>
2024-01-09 16:07:23 +0700

    add sorter

--- src/main/java/com/vtoan1517/controller/web/ArticleController.java
+++ src/main/java/com/vtoan1517/controller/web/ArticleController.java
@@ -4,8 +4,12 @@ import com.vtoan1517.dto.ArticleDTO;
 import com.vtoan1517.entity.Role;
 import com.vtoan1517.exception.ArticleNotFoundException;
 import com.vtoan1517.service.IArticleRetrievalService;
+import com.vtoan1517.service.IReviewRetrievalService;
 import com.vtoan1517.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,10 +23,13 @@ public class ArticleController {
 
     private final IArticleRetrievalService articleService;
 
+    private final IReviewRetrievalService reviewRetrievalService;
+
 
     @Autowired
-    public ArticleController(IArticleRetrievalService articleService) {
+    public ArticleController(IArticleRetrievalService articleService, IReviewRetrievalService reviewRetrievalService) {
         this.articleService = articleService;
+        this.reviewRetrievalService = reviewRetrievalService;
     }
 
     @GetMapping("/{slug}")
@@ -39,6 +46,8 @@ public class ArticleController {
 
         ModelAndView mav = new ModelAndView("web/details");
         mav.addObject("article", articleDTO);
+        Pageable pageable = new PageRequest(0, 2, new Sort(Sort.Direction.DESC, "createdDate"));
+        mav.addObject("comments", reviewRetrievalService.findAllByArticleSlug(slug, pageable));
         return mav;
     }
 }
